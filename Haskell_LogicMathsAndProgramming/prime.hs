dd1b4cbe35b794e0af39b5f018fb3b2f38e7f1f2
funcylambda <lambda@edwardhaigh.com>
2018-09-26 21:30:54 +0100

    Further work on ch.1

--- Haskell_LogicMathsAndProgramming/prime.hs
+++ Haskell_LogicMathsAndProgramming/prime.hs
@@ -2,10 +2,27 @@ module GS
 
 where
 
+divides :: Integer -> Integer -> Bool
 divides d n = rem n d == 0
 
+ld :: Integer -> Integer
 ld n = ldf 2 n 
 
+ldf :: Integer -> Integer -> Integer
 ldf k n | divides k n = k
         | k ^ 2 > n   = n
         | otherwise   = ldf (k+1) n
+
+prime0 :: Integer -> Bool
+prime0 n | n < 1     = error "not a positive integer"
+         | n == 1    = False
+         | otherwise = ld n == n
+
+mnmInt :: [Int] -> Int
+mnmInt []     = error "empty list"
+mnmInt [x]    = x
+mnmInt (x:xs) = min x (mnmInt xs)
+
+min' :: Int -> Int -> Int
+min' x y | x <= y    = x
+         | otherwise = y
